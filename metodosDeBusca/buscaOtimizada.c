b2d0fdcaf679be7c8e4c6831e6d6a6efed1a8ba9
Claudioalvesdev <claudioalvesdev@gmail.com>
2022-05-03 11:07:33 -0300

    Update 03.05.22

---
+++ metodosDeBusca/buscaOtimizada.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ordenarVetor(int tamanho, int *vetor);
+
+int buscaOtimizada(int tamanho, int *vetor, int valorDesejado);
+
+int main() {
+	int vetor[100], valorDesejado, indiceDesejado;
+	
+	printf("\n\t\t\t\tGERANDO UM VETOR COM TAMANHO 100\n\n");
+	
+	for(int i = 0; i < 100; i++) {
+		vetor[i] = rand() % 150;
+		
+		printf("[%3d] ", vetor[i]);
+	}
+	
+	ordenarVetor(100, vetor);
+	
+	printf("\n\t\t\t\t\tORDENANDO O VETOR COM INSERT SORT\n\n");
+	
+	for(int i = 0; i < 100; i++) {
+		printf("[%3d] ", vetor[i]);
+	}
+	
+	printf("\nDesejar procurar qual numero? ");
+	scanf("%d", &valorDesejado);
+	
+	indiceDesejado = buscaOtimizada(100, vetor, valorDesejado);
+	
+	if(indiceDesejado < 0) {
+		printf("[ERRO] Numero nao encontrado x(\n");
+	} else {
+		printf("Encontrado o numero %d no indice: %d\n", valorDesejado, indiceDesejado);
+	}
+	
+	return 0;
+}
+
+void ordenarVetor(int tamanho, int *vetor) {
+	int aux;
+	
+	for(int i = 0; i < tamanho; i++) {
+		aux = vetor[i];
+		
+		for(int j = i; j != 0; j--) {
+			if(vetor[j - 1] > vetor[j]) {
+				vetor[j] = vetor[j - 1];
+				vetor[j - 1] = aux;
+			} else {
+				break;
+			}
+		}
+	}
+}
+
+int buscaOtimizada(int tamanho, int *vetor, int valorDesejado) {
+	if(vetor[tamanho - 1] >= valorDesejado) {
+		if(vetor[tamanho - 1] == valorDesejado) {
+			return tamanho - 1;
+		}
+		
+		for(int i = 0; i < tamanho; i++) {
+			if(vetor[i] == valorDesejado) {
+				return i;
+			}
+		}
+	} else {
+		return -1;
+	}
+}

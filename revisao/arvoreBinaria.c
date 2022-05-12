1c080938e8d9c054a6cd05ed3ba243d7fd5b02a7
Antonio Claudio <87538025+kladDev@users.noreply.github.com>
2022-05-12 14:32:28 -0300

    Update 12.05.22

--- revisao/arvoreBinaria.c
+++ revisao/arvoreBinaria.c
@@ -9,6 +9,51 @@ struct no {
 
 typedef struct no No;
 
+No *inicializarArvore();
+
+No *inserirElemento(No *elemento, int valor) ;
+
+void mostrarElemento(No *elemento);
+
+No *removerNo(No *elemento, int valor) {
+    if(elemento == NULL) {
+        return NULL;
+    } else if(elemento->valor > valor) {
+        elemento->esquerda = removerNo(elemento->esquerda, valor);
+    } else if(elemento->valor < valor) {
+        elemento->direita = removerNo(elemento->direita, valor);
+    } else {
+        
+    // NÓ FOLHA
+    if(elemento->esquerda == NULL && elemento->direita == NULL) {
+        free(elemento);
+        
+        return NULL;
+    } else if(elemento->esquerda == NULL) {
+        No *aux = elemento;
+        elemento = elemento->direita;
+        free(t);
+        
+        return elemento;
+    } else if(elemento->direita == NULL) {
+        No *aux = elemento;
+        elemento = elemento->esquerda;
+        free(aux);
+        
+        return elemento;
+    }
+}
+
+int main() {
+	No *elemento = inicializarArvore();
+	elemento = inserirElemento(elemento, 10);
+	elemento = inserirElemento(elemento, 5);
+	elemento = inserirElemento(elemento, 2);
+	
+	mostrarElemento(elemento);
+	return 0;
+}
+
 No *inicializarArvore() {
 	return NULL;
 }
@@ -37,14 +82,4 @@ void mostrarElemento(No *elemento) {
 		mostrarElemento(elemento->direita);
 		printf("%d ", elemento->valor);
 	}
-}
-
-int main() {
-	No *elemento = inicializarArvore();
-	elemento = inserirElemento(elemento, 10);
-	elemento = inserirElemento(elemento, 5);
-	elemento = inserirElemento(elemento, 2);
-	
-	mostrarElemento(elemento);
-	return 0;
 }
